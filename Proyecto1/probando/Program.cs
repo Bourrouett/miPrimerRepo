8c0423e44d1d0e04a1e253748ca05e4caa7e8f0c
Bourrouett <manuel.bourrouett@gmail.com>
2025-03-09 00:52:33 -0600

    cadena nueva connectio

--- Proyecto1/probando/Program.cs
+++ Proyecto1/probando/Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine("Hello, Platzi from Manuel!");
 
 string stripe_api_key= "sk_test_4eC39HqLyjWDarjtT1zdp7dc";
+
+string testingConnection = "Server=tcp:manuserver.database.windows.net;1433;User Id=myUsername;Password=myPassword;"
